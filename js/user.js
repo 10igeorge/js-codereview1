d1adac64ee344cfb000991811cea2937d9fe65ce
10igeorge <10izzygeorge@gmail.com>
2016-03-18 11:04:24 -0700

    add hr tag betweeen each repo

--- js/user.js
+++ js/user.js
@@ -5,13 +5,14 @@ exports.getRepos = function(user){
     console.log(response);
     $('#results').show();
     $('#userImageResult').append("<img id='userImage' src='"+response.avatar_url+"'</img>");
-    $('#userNameResult').append("<h3"+response.name+"</h3>");
     $('#userLoginResult').append("<h3><a href='https://github.com/"+response.login+"'>"+response.login+"</a></h3>");
     $.get('https://api.github.com/users/'+user+'/repos?access_token=' + apiKey).then(function(repo){
       for(var i=0; i < repo.length; i++){
-        $('#repoNameResult').append("<p><a href='https://github.com/10igeorge/"+repo[i].name+"'>"+repo[i].name+"</p>");
         if (repo[i].description.length != 0){
-          $('#repoNameResult').append("<li id='info'>"+repo[i].description+"</li>");
+          $('#repoNameResult').append("<p><a href='https://github.com/10igeorge/"+repo[i].name+"'>"+repo[i].name+"</p>");
+          $('#repoNameResult').append("<p id='info'>"+repo[i].description+"</p><hr>");
+        } else {
+          $('#repoNameResult').append("<p><a href='https://github.com/10igeorge/"+repo[i].name+"'>"+repo[i].name+"</p><hr>");
         }
       }
     });
