ff4b3010b42108ccd2609c6412d43941eac26fef
10igeorge <10izzygeorge@gmail.com>
2016-03-18 10:51:43 -0700

    add link to user profile

--- js/user.js
+++ js/user.js
@@ -6,10 +6,10 @@ exports.getRepos = function(user){
     $('#results').show();
     $('#userImageResult').append("<img id='userImage' src='"+response.avatar_url+"'</img>");
     $('#userNameResult').append("<h3"+response.name+"</h3>");
-    $('#userLoginResult').append("<h3>"+response.login+"</h3>");
+    $('#userLoginResult').append("<h3><a href='https://github.com/"+response.login+"'>"+response.login+"</a></h3>");
     $.get('https://api.github.com/users/'+user+'/repos?access_token=' + apiKey).then(function(repo){
       for(var i=0; i < repo.length; i++){
-        // $('#repoNameResult').append("<p><a href='https://github.com/10igeorge/"+repo[i].name+"'>"+repo[i].name+"</p>");
+        $('#repoNameResult').append("<p><a href='https://github.com/10igeorge/"+repo[i].name+"'>"+repo[i].name+"</p>");
         if (repo[i].description.length != 0){
           $('#repoNameResult').append("<li id='info'>"+repo[i].description+"</li>");
         }
