98f128f609e88ae388380243fc5ecde593ef2b4c
10igeorge <10izzygeorge@gmail.com>
2016-03-18 11:51:10 -0700

    install and format join date with moment.js

--- js/user.js
+++ js/user.js
@@ -4,7 +4,7 @@ exports.getRepos = function(user){
   $.get('https://api.github.com/users/'+user+'?access_token=' + apiKey).then(function(response){
     console.log(response);
     $('#results').show();
-    $('#results').append("<img id='userImage' src='"+response.avatar_url+"'</img><h3><a href='https://github.com/"+response.login+"'>"+response.login+"</a></h3><h4>"+response.name+"</h4><ul class='list-inline'>Followers: </ul><li>"+response.followers+"</li><li>"+response.following+"</li><h5>Public Repositories: </h5>");
+    $('#results').append("<img id='userImage' src='"+response.avatar_url+"'</img><h3><a href='https://github.com/"+response.login+"'>"+response.login+"</a></h3><br><h4>"+response.name+"</h4><dl class='dl-horizontal'><dt>Followers</dt><dd>"+response.followers+"</dd><dt>Following</dt><dd>"+response.following+"</dd></dl><br><p id='joinDate'>Joined: "+moment(response.created_at).format('LL')+"</p><h5>Public Repositories: </h5><br>");
     $.get('https://api.github.com/users/'+user+'/repos?access_token=' + apiKey).then(function(repo){
       for(var i=0; i < repo.length; i++){
         if (repo[i].description.length != 0){
