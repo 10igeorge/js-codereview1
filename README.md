78dffc1cc22b09ebcb8be0b95fe5a6e4a8fe7c2a
10igeorge <10izzygeorge@gmail.com>
2016-03-18 12:17:32 -0700

    update readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# GitHub API
+
+#### Created By
+Isabelle George
+
+## Description
+This project was for a code review for the Epicodus JavaScript class that features the GitHub API. The goal of the project was to retrieve user information using the GitHub API and display an image, a user profile, and a list of public repositories create by the user.
+
+Enjoy!
+
+###Technologies
+- HTML/CSS
+- JavaScript
+- jQuery
+- Node/Gulp
+- Bower
+
+#### Setup/Installation Requirements
+
+** See required technologies above **
+In the terminal:
+```
+$ git clone https://github.com/10igeorge/js-codereview1.git
+```
+While in git directory, run:
+```
+$ npm install
+$ bower install
+$ gulp serve
+```
+
+##### License
+
+Copyright (c) 2016 Isabelle George
--- js/user.js
+++ js/user.js
@@ -5,7 +5,7 @@ exports.getRepos = function(user){
     console.log(response);
     $('#results').show();
     $('#results').append("<img id='userImage' src='"+response.avatar_url+"'</img><h3><a href='https://github.com/"+response.login+"'>"+response.login+"</a></h3><h4>"+response.name+"</h4><dl class='dl-horizontal'><dt>Followers</dt><dd>"+response.followers+"</dd><dt>Following</dt><dd>"+response.following+"</dd></dl><br><p id='joinDate'>Joined: "+moment(response.created_at).format('LL')+"</p><h5>Public Repositories: </h5><br>");
-    $.get('https://api.github.com/users/'+user+'/repos?access_token=' + apiKey).then(function(repo){
+    $.get('https://api.github.com/users/'+user+'/repos?direction=desc&access_token=' + apiKey).then(function(repo){
       for(var i=0; i < repo.length; i++){
         if (repo[i].description.length != 0){
           $('#results').append("<p><a href='https://github.com/10igeorge/"+repo[i].name+"'>"+repo[i].name+"</a></p><p id='repoInfo'>Created: "+moment(repo[i].created_at).format('LL')+"</p><p id='repoInfo'>Description: "+repo[i].description+"</p><hr>");
