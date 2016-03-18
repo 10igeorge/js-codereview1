0ded05616590b3209620bb5f8fd630d705b51d7c
10igeorge <10izzygeorge@gmail.com>
2016-03-18 14:11:57 -0700

    account for a user with no name inputted

--- js/user.js
+++ js/user.js
@@ -5,7 +5,12 @@ exports.getRepos = function(user){
   $.get('https://api.github.com/users/'+user+'?access_token=' + apiKey).then(function(response){
     console.log(response);
     $('#results').show();
-    $('#results').append("<img id='userImage' src='"+response.avatar_url+"'</img><h3><a href='https://github.com/"+response.login+"'>"+response.login+"</a></h3><h4>"+response.name+"</h4><dl class='dl-horizontal'><dt>Followers</dt><dd>"+response.followers+"</dd><dt>Following</dt><dd>"+response.following+"</dd></dl><p id='joinDate'>Joined: "+moment(response.created_at).format('LL')+"</p><br><h5>Public Repositories: </h5>");
+    if (response.name !== null){
+      $('#results').append("<img id='userImage' src='"+response.avatar_url+"'</img><h3><a href='https://github.com/"+response.login+"'>"+response.login+"</a></h3><h4>"+response.name+"</h4><dl class='dl-horizontal'><dt>Followers</dt><dd>"+response.followers+"</dd><dt>Following</dt><dd>"+response.following+"</dd></dl><p id='joinDate'>Joined: "+moment(response.created_at).format('LL')+"</p><br><h5>Public Repositories: </h5>");
+    } else {
+      $('#results').append("<img id='userImage' src='"+response.avatar_url+"'</img><h3><a href='https://github.com/"+response.login+"'>"+response.login+"</a></h3><dl class='dl-horizontal'><dt>Followers</dt><dd>"+response.followers+"</dd><dt>Following</dt><dd>"+response.following+"</dd></dl><p id='joinDate'>Joined: "+moment(response.created_at).format('LL')+"</p><br><h5>Public Repositories: </h5>");
+    }
+
     $.get('https://api.github.com/users/'+user+'/repos?direction=desc&access_token=' + apiKey).then(function(repo){
       for(var i=0; i < repo.length; i++){
         if (repo[i].description.length !== 0){
