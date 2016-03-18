62f2b54dbcf6cb0edab7839551d4528a749ab5ae
10igeorge <10izzygeorge@gmail.com>
2016-03-18 13:47:40 -0700

    edit html page

--- js/user.js
+++ js/user.js
@@ -1,5 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 
+
 exports.getRepos = function(user){
   $.get('https://api.github.com/users/'+user+'?access_token=' + apiKey).then(function(response){
     console.log(response);
